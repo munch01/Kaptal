6560985f0fbfa8de2305eb9b0b2774b3711c3c10
munch01 <emeric.gaugain@protonmail.com>
2026-08-13 14:07:39 +0200

    pour test google

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.Muncho.kaptal"
         minSdk = 26
         targetSdk = 35
-        versionCode = project.findProperty("versionCode")?.toString()?.toInt() ?: 1
-        versionName = project.findProperty("versionName")?.toString() ?: "1.0"
+        versionCode = project.findProperty("versionCode")?.toString()?.toInt() ?: 2
+        versionName = project.findProperty("versionName")?.toString() ?: "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
