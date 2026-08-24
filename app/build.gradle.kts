5e027708453392d1063af2683204546d0e7c6415
munch01 <emeric.gaugain@protonmail.comgit config --global user.name>
2026-08-24 19:40:39 +0200

    Debug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.Muncho.kaptal"
         minSdk = 26
         targetSdk = 36
-        versionCode = 24
-        versionName = "1.0.24"
+        versionCode = 124
+        versionName = "1.0.124"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
